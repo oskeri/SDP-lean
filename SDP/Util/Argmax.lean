b5b04dd9b7c17ba9edff2aee539bd3bd2ae9a466
Oskar Eriksson <oskeri@chalmers.se>
2025-03-11 13:18:58 +0100

    Finish the definition of Argmax

--- SDP/Util/Argmax.lean
+++ SDP/Util/Argmax.lean
@@ -13,46 +13,60 @@ class Argmax (A B : Type) extends Preorder B where
   argmax : (A → B) → A
   le_argmax (f : A → B) (a : A) : f a ≤ f (argmax f)
 
-namespace Argmax
+namespace List
 
-/-- A function used to define an `Argmax` instance. -/
+/-- A version of `argmax` for non-empty lists. -/
 
-def argmax' [FinEnum A] [Nonempty A] [Preorder B] [@DecidableRel B B (· < ·)] (f : A → B) : A :=
-  match h : List.argmax f (FinEnum.toList A) with
-    | some a => a
-    | none => by
-      exfalso
-      apply @FinEnum.toList_neq_nil A
-      rw [List.argmax_eq_none] at h
-      assumption
+def argmax_nonempty [Preorder B] [@DecidableRel B B (· < ·)]
+  (f : A → B) (l : List A) (nonempty : l ≠ []) : A :=
+  match h : argmax f l with
+  | some a => a
+  | none => by
+    exfalso
+    apply nonempty
+    rw [argmax_eq_none] at h
+    assumption
 
-lemma argmax'_mem_argmax [FinEnum A] [Nonempty A] [Preorder B] [@DecidableRel B B (· < ·)]
-  (f : A → B) : argmax' f ∈ List.argmax f (FinEnum.toList A) := sorry
+/-- `argmax_nonempty` gives the argument that maximizes `f` of all
+elements in the list. -/
 
-  -- unfold argmax'
-  -- match h : List.argmax f (FinEnum.toList A) with
-  -- | some a =>
-  --   unfold argmax'
-  --   simp [h]
+theorem le_argmax_nonempty [TotalPreorder B] (f : A → B) (l : List A) (nonempty : l ≠ []) :
+  a ∈ l → f a ≤ f (argmax_nonempty f l nonempty) := by
+  intro mem
+  match @trichotomous B (· < ·) _ (f a) (f (argmax_nonempty f l nonempty)) with
+  | .inl h =>
+    apply le_of_lt
+    assumption
+  | .inr (.inl h) =>
+    rw [h]
+  | .inr (.inr h) =>
+    exfalso
+    apply not_lt_of_mem_argmax _ _ h
+    · exact l
+    · exact mem
+    · unfold argmax_nonempty
+      split
+      · case h_1 a h' =>
+        rw [h']
+        trivial
+      · case h_2 h' =>
+        exfalso
+        apply nonempty
+        rw [argmax_eq_none] at h'
+        assumption
 
-  -- | none => sorry
+end List
+
+namespace FinEnum
 
 /-- An instance of `Argmax A B` for non-empty, finite enumerable type `A` and totally
 preordered type `B`. -/
 
-instance argmax_of_finEnum [FinEnum A] [Nonempty A] [TotalPreorder B] : Argmax A B where
-  argmax := argmax'
-  le_argmax f a := by
-    match @trichotomous B (· < ·) _ (f a) (f (argmax' f)) with
-      | .inl h =>
-        apply le_of_lt
-        assumption
-      | .inr (.inl h) => rw [h]
-      | .inr (.inr h) =>
-        exfalso
-        apply List.not_lt_of_mem_argmax _ _ h
-        · exact FinEnum.toList A
-        · apply FinEnum.mem_toList
-        · apply argmax'_mem_argmax
+instance [FinEnum A] [Nonempty A] [TotalPreorder B] : Argmax A B where
+  argmax f :=
+    List.argmax_nonempty f (FinEnum.toList A) FinEnum.toList_neq_nil
+  le_argmax f a :=
+    List.le_argmax_nonempty f (FinEnum.toList A)
+      FinEnum.toList_neq_nil (FinEnum.mem_toList a)
 
-end Argmax
+end FinEnum
