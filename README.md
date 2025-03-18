6835ffe011a4ba31f9c75857c24ffe2d44a905f5
Oskar Eriksson <oskeri@chalmers.se>
2025-03-18 15:32:07 +0100

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # SDP-lean
+
+A small library for solving sequential decision problems in Lean based on [Responsibility Under Uncertainty: Which Climate Decisions Matter Most?](https://doi.org/10.1007/s10666-022-09867-w) by Botta et al. (as well as preceeding papers).
+
+See `SDP.lean` for an overview of the project.
--- SDP.lean
+++ SDP.lean
@@ -1,18 +1,38 @@
 -- This module serves as the root of the `SDP` library.
--- Import modules here that should be built as part of the library.
-import SDP.Examples.GHG
-import SDP.Examples.TicTacToe
-import SDP.Monad.SP
-import SDP.Policy
-import SDP.Policy.Argmax
-import SDP.Policy.FinEnum
-import SDP.SDP
-import SDP.Trajectory
-import SDP.Value
+
+-- Definition of the `Argmax` typeclass
 import SDP.Util.Argmax
+-- `Argmax` instance for nonempty, finitely enumerable types
 import SDP.Util.FinEnum
+-- Definition of total, decidable preorders.
 import SDP.Util.Order
+
+-- A monad instance
+import SDP.Monad.SP
+
+-- Definitions of Values and Values with a `measure` function-
+import SDP.Value
+
+-- `Value` and `Measure` instances
 import SDP.Value.Nat
 import SDP.Value.Nat.Max
 import SDP.Value.Rat
 import SDP.Value.Real
+
+-- Definition of SDP:s
+import SDP.SDP
+
+-- Definition of policies, policy sequences and solving SDP:s
+-- (finding optimal policy sequences) using backwards induction
+import SDP.Policy
+-- Solving SDP:s using an `argmax` function
+import SDP.Policy.Argmax
+-- Solving SDP:s for nonempty and finitely enumerable control spaces.
+import SDP.Policy.FinEnum
+
+-- Definition of Trajectories and displaying SDP solutions.
+import SDP.Trajectory
+
+-- Some examples
+import SDP.Examples.GHG
+import SDP.Examples.TicTacToe
